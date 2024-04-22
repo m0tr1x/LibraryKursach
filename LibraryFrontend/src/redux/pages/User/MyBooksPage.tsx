ecce0dbc171f314b40d3b7ab9c0a77f91faa1064
m0tr1x <grigorysyzran@mail.ru>
2024-04-22 22:28:02 +0300

    done now

--- LibraryFrontend/src/redux/pages/User/MyBooksPage.tsx
+++ LibraryFrontend/src/redux/pages/User/MyBooksPage.tsx
@@ -1,16 +1,15 @@
-import {Col, Row} from "react-bootstrap";
+import {CardFooter, CardHeader, Col, ListGroupItem, Row} from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import {useEffect, useState} from "react";
 import {IBook} from "../../Interfaces/IBook.tsx";
-import { Button } from 'react-bootstrap';
-import { getMyBooks, returnBook} from "../../../api/api.tsx";
+import {Button} from 'react-bootstrap';
+import {getMyBooks, returnBook} from "../../../api/api.tsx";
 
-export function MyBooksPage()
-{
+export function MyBooksPage() {
     const [myBooks, setMyBooks] = useState<IBook[]>([]);
 
 
-    const handleReturnBook = async (bookId:number) => {
+    const handleReturnBook = async (bookId: number) => {
         try {
             await returnBook(bookId);
             // Обработка успешного взятия книги
@@ -22,7 +21,7 @@ export function MyBooksPage()
     };
     const loadMyBooksBooks = async () => {
         try {
-            const books =  await getMyBooks(); // Получаем доступные книги
+            const books = await getMyBooks(); // Получаем доступные книги
             setMyBooks(books); // Обновляем состояние с данными о книгах
         } catch (error) {
             console.error('Ошибка при загрузке доступных книг:', error.message);
@@ -35,20 +34,25 @@ export function MyBooksPage()
         loadMyBooksBooks();
     }, []);
 
-    return(
+    return (
         <>
-            <div style={{marginLeft:'10px', marginRight:'10px'}}>
-                <h1 style={{alignContent:"center"}}>Мои книги</h1>
-                <Row xs={1} md={2} lg={5}className="g-4" style={{marginTop:'15px'}}>
+            <div style={{marginLeft: '10px', marginRight: '10px'}}>
+                <h1 style={{alignContent: "center"}}>Мои книги</h1>
+                <Row xs={1} md={2} lg={5} className="g-4" style={{marginTop: '15px'}}>
                     {myBooks.map(book => (
                         <Col key={book.id}>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Body>
+                            <Card style={{width: '18rem'}}>
+                                <CardHeader>
                                     <Card.Title>{book.title}</Card.Title>
-                                    <Card.Text>
-                                        {book.authorName}
-                                    </Card.Text>
-                                    <Button variant="primary" onClick={() => handleReturnBook(book.id)} >Вернуть книгу</Button>
+                                </CardHeader>
+                                <Card.Body>
+                                        <Card.Text style={{display: 'flex', justifyContent: 'center'}}>
+                                            {book.author.name}
+                                        </Card.Text>
+                                    <CardFooter  style={{display: 'flex', justifyContent: 'center'}}>
+                                        <Button variant="primary" onClick={() => handleReturnBook(book.id)}>Вернуть
+                                            книгу</Button>
+                                    </CardFooter>
                                 </Card.Body>
                             </Card>
                         </Col>
